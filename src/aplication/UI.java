d6bc75e591696d548bfefda6178f1d42d8e85864
3dn1r4 <ednirf@gmail.com>
2020-07-03 11:23:13 -0300

    Mostrando jogador e turno

--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.SynchronousQueue;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -46,6 +47,13 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro carregamento da posição. Posição válida de a1 até h8.");
 		}
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turno: " + chessMatch.getTurn());
+		System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
